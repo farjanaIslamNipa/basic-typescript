005ecbbb97930b4fd5ecb7d6a947f958e15c49af
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-11-05 16:26:56 +0600

    working on named and type alias

--- src/alias.ts
+++ src/alias.ts
@@ -1,6 +1,46 @@
 {
 
+  // type alias
+  type Person = {  // Here Person is type alias
+    id: number,
+    name: string,
+    roll: string,
+    phone: string,
+    address?: string
+  }
 
+  const student1 : Person = {
+    id: 1,
+    name: 'Mr. X',
+    roll: 'manager',
+    phone: '01754896',
+    address: 'adfdasf'
+  } 
+  const student2 : Person = {
+    id: 2,
+    name: 'Mr. Y',
+    roll: 'Director',
+    phone: '01754897',
+    address: 'adfdasf'
+  } 
+  const student3 : Person = {
+    id: 3,
+    name: 'Mr. Z',
+    roll: 'CEO',
+    phone: '01754898',
+    address: 'adfdasf'
+  } 
+
+
+  // type alias in function
+  type Add = (a:number, b:number) => number // Here Add is function alias
+
+  const sum : Add = (x, y) => x + y;
+
+  sum(10, 30)
+
+
+  // name alias
 type User = {
   id: number,
   name: {
@@ -21,7 +61,7 @@ const user1 : User = {
   contactNo: 12346
 }
 
-const {contactNo : phone, name: {mName : midName}} = user1 // here phon and midName are alias
+const {contactNo : phone, name: {mName : midName}} = user1 // here phone and midName are name alias
 console.log(phone, midName)
 
 }
--- src/destructuring.ts
+++ src/destructuring.ts
@@ -1,5 +1,31 @@
 {
-
-
+  type User = {
+    id: number,
+    name: {
+      fName: string,
+      mName: string,
+      lName: string,
+    },
+    contactNo: number
+  }
   
+  const user1 : User = {
+    id: 22,
+    name: {
+      fName: 'ab',
+      mName: 'cd',
+      lName: 'ef'
+    },
+    contactNo: 12346
+  }
+  
+  const {contactNo : phone, name: {mName : midName}} = user1 // here phon and midName are alias
+  console.log(phone, midName)
+  
+const myFriends : string[] = ['dina', 'mayna', 'sabbir', 'tarun', 'priyo', 'sohel', 'rana']
+
+const [a, b, bestu] = myFriends
+const [,, besti, , ...rest] = myFriends
+
+
 }
