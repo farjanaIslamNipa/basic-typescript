0edc96f044ed0f64667b8d9af458052d117bc408
farjanaIslamNipa <nipa.farzana87@gmail>
2023-11-07 12:33:15 +0600

    working on type guard

--- src/type-guard.ts
+++ src/type-guard.ts
@@ -83,10 +83,18 @@ class Cat extends Animal {   //Here Cat is inheritance
   }
 }
 
+const isDog = (animal: Animal) : animal is Dog => {
+  return animal instanceof Dog
+}
+
+const isCat = (animal: Animal) : animal is Cat => {
+  return animal instanceof Cat
+}
+
 const getAnimal = (animal: Animal) => {
-  if(animal instanceof Dog) {
+  if(isDog(animal)) {
     animal.makeBark()
-  }else if(animal instanceof Cat) {
+  }else if(isCat(animal)) {
     animal.makeMeow()
   }else{
     animal.makeSound()
