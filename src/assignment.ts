aaef5b93250d8c485615cc9a6c5ca793bbea027e
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-11-08 15:22:14 +0600

    type assertion complete

--- src/assignment.ts
+++ src/assignment.ts
@@ -80,9 +80,9 @@ const checkSpecies = (petAnimal : Cat) : string => {
   }else return "No, it's not a cat"
 }
 
-let checkCat = checkSpecies(petCat) 
+// let checkCat = checkSpecies(petCat) 
 
-console.log(checkCat)
+// console.log(checkCat)
 
 // class PetAnimal {
 //   constructor(public name: string, public color: string){}
@@ -121,16 +121,32 @@ console.log(checkCat)
 
 
 
-// You got a list that has numbers and words mixed together. Your job is to make a function that will take the list as an argument and return the total by adding them up.
+// You got a list that has numbers and words mixed together. Your job is to make a function that will take the list as an argument and return the sum by adding them up.
 
 // To solve this in TypeScript, you'll look at each thing in the list named mixedData, which looks like this: [1, 'two', 3, 'four', 5]. You'll go through the list, find the numbers, and add them together.
 
-// Every time you find a number in the list, you'll add it to a total. You'll start at zero and then keep adding the numbers you find. If no number is found in the list return 0. To make sure TypeScript knows these things are numbers, you'll use type assertions.
+// Every time you find a number in the list, you'll add it to a sum. You'll start at zero and then keep adding the numbers you find. If no number is found in the list return 0. To make sure TypeScript knows these things are numbers, you'll use type assertions.
 
 // Problem 4
 
+type NumberOrWord = number | string
+
+let mixedData : NumberOrWord[] = [ 10, 'ten', 20, 'twenty', 30, 'thirty', 0 ]
+
+const getSumOfNumbers = (mixedList : NumberOrWord[]) : number => {
+  let sum = 0
+  for (let i = 0; i < mixedList.length; i++) {
+    const element = mixedList[i];
+    if(typeof element === 'number'){
+      sum += element
+    }
+  }
+  return sum as number;
+}
 
 
+const sumNum = getSumOfNumbers(mixedData)
+console.log(sumNum)
 
 
 }
